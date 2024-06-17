2ac80a5504739e776d6ac20212f2a00d3a5fcd97
Nrorn <sermyagin93@gmail.com>
2024-06-17 21:54:55 +0300

    пишу скрипт для удаляения пакета из aptly repo

--- doc/test_work/aptly_dalete.py
+++ doc/test_work/aptly_dalete.py
@@ -32,3 +32,41 @@ def run_bash_command(repo_name, package_path):
 
 # Выполняем команду bash
     #run_bash_command(repo_name, package_path)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    import subprocess
+import sys
+
+def run_bash_command(repo_name, package_path):
+    # Формируем команду со встроенной частью и аргументами
+    command = ["aptly", "repo", "add", repo_name, package_path]
+    
+    # Выполняем команду
+    result = subprocess.run(command, capture_output=True, text=True)
+    
+    # Выводим результат
+    print(f"stdout:\n{result.stdout}")
+    print(f"stderr:\n{result.stderr}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python run_command.py <repo_name> <package_path>")
+        sys.exit(1)
+    
+    # Читаем аргументы командной строки
+    repo_name = sys.argv[1]
+    package_path = sys.argv[2]
+    
+    # Выполняем команду bash
+    run_bash_command(repo_name, package_path)
